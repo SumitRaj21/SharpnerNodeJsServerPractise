a5f68bf47d39834b882dea7e2cb4a425bb53e8f6
SumitRaj21 <sumitraj9119@gmail.com>
2024-11-11 13:01:17 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const http=require('http');
+const server=http.createServer((req,res)=>{
+    const url=req.url;
+    if(url==='/home'){
+        res.write('<html>')
+        res.write('<head><title>My First Message</title></head>')
+        res.write('<body>Welcome Home</body>')
+        res.write('</html>')
+        return res.end();
+    }else  if(url==='/about'){
+        res.write('<html>')
+        res.write('<head><title>My First Message</title></head>')
+        res.write('<body>Welcome to About Us page</body>')
+        res.write('</html>')
+        return res.end();
+    }else if(url==='/node'){
+        res.write('<html>')
+        res.write('<head><title>My First Message</title></head>')
+        res.write('<body>Welcome to my Node Js project</body>')
+        res.write('</html>')
+        return res.end();
+    }
+})
+server.listen(3000);
